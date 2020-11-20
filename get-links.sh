2d77c50c6d9a7281d56afd80706dcf2166f6b0ef
Brandon Jones <brandon.jones@cerner.com>
2020-11-20 17:13:31 -0600

    Very sloppy first run

---
+++ get-links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for beer in $(cat ./categories); do
+  echo "" > ./links/${beer}.txt
+  for i in {1..36}; do
+    curl -s "https://carttocar.com/bier-station/?product-page=${i}" | grep -A 2 instock | egrep -A 2 "product_cat.*${beer}" | grep href | cut -f2 -d \" | egrep "pack\/$|pk\/$" | egrep -v "non-alcoholic|nonalcoholic" | sort | uniq >> ./links/${beer}.txt
+  done
+done
